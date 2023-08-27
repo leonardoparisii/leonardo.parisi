74c3c1ac694870f0b0c6f50eda399688f4950cc2
G3n3sis-x <parisileonardo15@gmail.com>
2023-08-27 14:04:33 +0200

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,14 +7,14 @@ const Navbar = () => {
   return (
     <div className="grid md:grid-cols-3 grid-cols-2 place-items-center px-10 py-4 w-full">
         <div className="w-full flex justify-start">
-            <Link href="/"  className="font-source text-2xl font-bold text-white cursor-pointer">
+            <Link href="/"  className="font-source text-xl md:text-2xl font-bold text-white cursor-pointer">
                 Leonardo Parisi
             </Link>
         </div>
         <div className="flex justify-center gap-4 md:rounded-none rounded-full md:border-none border md:relative fixed md:bottom-0 bottom-16 md:p-0 md:px-0 p-2 px-4 border-slate-800 md:bg-transparent bg-slate-800/70 md:backdrop-blur-none backdrop-blur-3xl md:w-full w-fit">
             {data.navData.map((item, i) => (
                 <Link key={i} href={item.url}>
-                    <div className="text-white text-sm font-semibold font-mulish hover:text-[#94A3B8] transition-colors">
+                    <div className="hover:text-white focus:text-white text-sm font-semibold font-mulish text-slate-300/70 transition-colors">
                         {item.text}
                     </div>
                 </Link>
