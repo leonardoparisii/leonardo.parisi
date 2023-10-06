f3fd5d55a99ca8935878eca6cf3ce8244d379c5d
Leonardo Parisi <parisii.leonardo>
2023-10-06 16:58:45 +0200

    commented getFilteredData (old design func)

--- src/actions/getFilteredData.ts
+++ src/actions/getFilteredData.ts
@@ -1,11 +1,11 @@
 import data from "@/data/data.json";
 
 export function getFilteredData(filter: any) {
-    let filteredProjects = data.projects;
+  let filteredProjects = data.projects;
 
-    if (filter && filter !== "All") {
-        filteredProjects = data.projects.filter((project) => project.type === filter);
-    }
+  if (filter && filter !== "All") {
+    // filteredProjects = data.projects.filter((project) => project.type === filter);
+  }
 
-    return filteredProjects;
+  return filteredProjects;
 }
