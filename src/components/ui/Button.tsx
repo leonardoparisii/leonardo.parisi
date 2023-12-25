832d86b4ac4b5e57c99754f4d81a84e52b1e980f
Leonardo Parisi <parisii.leonardo>
2023-12-25 01:04:14 +0100

    small changes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,7 +5,7 @@ export default function Button({ title, onClick, customStyle }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={`${customStyle} bg-slate-700/20 w-fit py-2 px-3.5 cursor-pointer rounded-lg text-sm font-semibold border border-slate-700/70 box-border transition-colors hover:bg-white hover:text-black hover:border-slate-500/50`}
+      className={`${customStyle} transition-all bg-slate-700/20 w-fit py-2 px-3.5 cursor-pointer rounded-lg text-sm text-black font-semibold border border-slate-700/70 box-border transition-colors hover:bg-white hover:text-black hover:border-slate-500/50`}
     >
       {title}
     </button>
